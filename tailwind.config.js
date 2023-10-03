4c33b7cc20f67729209845b2e74bc7c267be8cf3
Joseph Fay <joegfay@gmail.com>
2023-10-03 11:43:14 -0500

    initial

---
+++ tailwind.config.js
@@ -0,0 +1,10 @@
+module.exports = {
+  content: [
+    './src/**/*.{js,ts,jsx,tsx,mdx}',
+    './src/components/**/*.{js,ts,jsx,tsx,mdx}',
+    './app/**/*.{js,ts,jsx,tsx,mdx}',
+  ],
+  theme: {},
+  variants: {},
+  plugins: [],
+};
